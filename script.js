c165ed05d38abad31fb04191447baef6f8ec3cad
Kathryn <kathryncreek@gmail.com>
2023-11-26 13:07:58 +0000

    Fix file paths.

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 function toggleImage() {
-    let image = document.getElementById('selfie');
-    let currentImage = image.src;
-    if (currentImage.endsWith('/images/me.jpg')) {
-      image.src = '/images/minerva.jpg';
+    let image = document.getElementById('selfie')
+    let currentImage = image.src
+    if (currentImage.endsWith("me.jpg")) {
+      image.src = "./images/minerva.jpg"
     } else {
-      image.src = '/images/me.jpg';
+      image.src = "./images/me.jpg"
     }
 }
 
 function playClick() {
-    const audio = new Audio('./audio/mouse-click.mp3');
-    audio.play();
+    const audio = new Audio("./audio/mouse-click.mp3")
+    audio.play()
 }
 
-const soundSources = ['./audio/anime-wow.mp3','./audio/squeak.mp3'];
-let currentSoundIndex = 0;
+const soundSources = ["./audio/anime-wow.mp3" , "./audio/squeak.mp3"]
+let currentSoundIndex = 0
 function playSilly() {
-    let audio = new Audio(soundSources[currentSoundIndex]);
-    audio.play();
-    currentSoundIndex = (currentSoundIndex + 1) % soundSources.length;
+    let audio = new Audio(soundSources[currentSoundIndex])
+    audio.play()
+    currentSoundIndex = (currentSoundIndex + 1) % soundSources.length
 }
 
-const rickRoll = new Audio('./audio/8-bit-rick-roll.mp3');
+const rickRoll = new Audio("./audio/rick-roll.mp3")
 function playRickRoll() {
     if (rickRoll.paused) {
-      rickRoll.play();
+      rickRoll.play()
     }
 }
 
 function pauseRickRoll() {
     if (rickRoll.play) {
-      rickRoll.pause();
+      rickRoll.pause()
     }
 }
 
